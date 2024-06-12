c1a7f67f27d17867036ae7a84c028df4c20e353c
G4boTr <gabriel_trillos@soy.sena.edu.co>
2024-06-12 02:18:58 -0500

    corregido js a jsx

---
+++ src/components/layout/Content.jsx
@@ -0,0 +1,18 @@
+import { Component } from "react";
+export default class Content extends Component {
+    constructor(props) {
+        super(props)
+        this.defaultMsj = "Elije una de las opciones...";
+    }
+    render() {
+        return(
+            <div className="Content">
+                <h1 style={ { fontSize: "85px", marginTop: "5px", zIndex: 25, marginLeft: "5vw", color: "#fff" } }
+                > { this.defaultMsj } </h1>
+            </div>
+        );
+    }
+};
+
+
+
